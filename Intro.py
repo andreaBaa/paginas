64a84fa60382dd337fb4fca1ce547fc232faaf17
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-02 10:51:55 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from PIL import Image
 
+st.title("Página inicial")
+
 image = Image.open("imagen.png")
 st.image(image, caption= "Imagen")
-st.title("Página inicial")
 
     
 
