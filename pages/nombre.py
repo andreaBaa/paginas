21ff76ee76a854f6132b134f14ef4d10e8dab911
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-18 17:06:57 -0500

    Update nombre.py

--- pages/nombre.py
+++ pages/nombre.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 
 # Título y Subtítulo
 st.title("¡Aprende lenguaje de señas colombiano!")
@@ -20,4 +21,34 @@ A continuación, encontrarás un video muy corto que enseña cómo saludar, deci
 
 # Video
 st.video("deletreonombre.mp4")
+
+# Subtítulo y Texto
+st.subheader("¡Ponlo en práctica!")
+st.write("""
+Escribe tu nombre y luego verás unas imágenes en desorden que corresponden a las letras de tu nombre. Con tus conocimientos previos del abecedario, identifica cada letra y ordénalas para conformar tu nombre en orden.
+""")
+
+# Input para escribir el nombre
+nombre = st.text_input("Escribe solo tu primer nombre (sin tildes)").upper()
+
+# Arreglo con las letras del abecedario
+abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+# Diccionario para mapear cada letra con su imagen correspondiente
+letras_imagenes = {}
+
+# Directorio donde se encuentran las imágenes
+directorio = "letras"
+
+# Iterar sobre cada letra y asignarle la imagen correspondiente
+for letra in abecedario:
+    imagen = f"{letra}.png"
+    ruta_imagen = os.path.join(directorio, imagen)
+    letras_imagenes[letra] = ruta_imagen
+
+# Mostrar las imágenes de las letras del nombre ingresado
+for letra in nombre:
+    if letra in letras_imagenes:
+        st.image(letras_imagenes[letra])
+
 
