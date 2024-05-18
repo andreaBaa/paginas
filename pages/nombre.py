a831fd5b6b4b497aec791276289a05cb1375e4cb
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-18 16:11:32 -0500

    Update nombre.py

--- pages/nombre.py
+++ pages/nombre.py
@@ -6,7 +6,7 @@ st.subheader("Básico: tu nombre")
 
 # Cuerpo de Texto
 st.write("""
-En la comunidad de personas sordas, la presentación de los nombres se realiza mediante el uso del alfabeto manual del lenguaje de señas, que vimos en el módulo anterior. Al presentarse, las personas sordas deletrean su nombre letra por letra utilizando cualquiera de sus dos manos. Este método de deletreo permite una comunicación clara y precisa, asegurando que el nombre sea entendido correctamente. 
+En la comunidad de personas sordas, la presentación de los nombres se realiza mediante el uso del alfabeto manual del lenguaje de señas, que vimos en el módulo anterior. Al presentarse, las personas sordas deletrean su nombre letra por letra utilizando cualquiera de sus dos manos. Este método de deletreo permite una comunicación clara y precisa, asegurando que el nombre sea entendido. 
 
 *Por ejemplo:* si una persona se llama "Ana" y quiere presentarse, deletreará  "A-N-A" en lenguaje de señas.
 """)
